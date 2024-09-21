f9ce522164de14971cdab8c2ea22136cf19595bb
mitugui <cristian.mitugui04@gmail.com>
2024-09-20 23:16:35 -0300

    feat: implementando inserção de conta no banco de dados - Refatorando ConexaoDB para ConnectionFactory para centralizar a criação de conexões e facilitar a manutenção. - Utilizando a classe PreparedStatement, a partir de uma instância de ConnectionFactory, para realizar um insert no banco.

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -6,8 +6,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDB {
-    public static void main(String ...x) {
+public class ConnectionFactory {
+    public Connection recuperarConexao() {
         Dotenv dotenv = Dotenv.load();
 
         String DB_HOST = dotenv.get("DB_HOST");
@@ -17,15 +17,11 @@ public class ConexaoDB {
         String DB_PASSWORD = dotenv.get("DB_PASSWORD");
 
         try {
-            Connection connection = DriverManager
+            return DriverManager
                     .getConnection(String.format("jdbc:mysql://%s:%s/%s?user=%s&password=%s",
                             DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD));
-
-            System.out.println("Recuperei a conexão");
-
-            connection.close();
         } catch (SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -1,13 +1,21 @@
 package br.com.alura.bytebank.domain.conta;
 
+import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
-import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
+    private ConnectionFactory connection;
+
+    public ContaService() {
+        this.connection = new ConnectionFactory();
+    }
 
     private Set<Conta> contas = new HashSet<>();
 
@@ -21,13 +29,27 @@ public class ContaService {
     }
 
     public void abrir(DadosAberturaConta dadosDaConta) {
-        var cliente = new Cliente(dadosDaConta.dadosCliente());
-        var conta = new Conta(dadosDaConta.numero(), cliente);
-        if (contas.contains(conta)) {
-            throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
-        }
+        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
+                "VALUES (?, ?, ?, ?, ?)";
 
-        contas.add(conta);
+        Connection conn = connection.recuperarConexao();
+
+        try {
+            PreparedStatement preparedStatement =  conn.prepareStatement(sql);
+
+            preparedStatement.setInt(1, dadosDaConta.numero());
+            preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
+            preparedStatement.setString(3, dadosDaConta.dadosCliente().nome());
+            preparedStatement.setString(4, dadosDaConta.dadosCliente().cpf());
+            preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
+
+            preparedStatement.execute();
+
+            preparedStatement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
